b0e70b2d3b97a7b2037f0d972baff5ee171f3b54
Zenit-yapper <zenitog69@gmail.com>
2026-03-28 22:04:59 +0530

    Update NexoraClient.java

--- src/main/java/com/nexora/client/NexoraClient.java
+++ src/main/java/com/nexora/client/NexoraClient.java
@@ -1,1 +1,20 @@
+package com.nexora.client;
+
+import com.nexora.client.hud.NexoraHud;
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.ModInitializer;
+
+public class NexoraClient implements ModInitializer, ClientModInitializer {
+    @Override
+    public void onInitialize() {
+        // Essential safety for PojavLauncher
+    }
+
+    @Override
+    public void onInitializeClient() {
+        // Start the HUD
+        NexoraHud.init();
+        System.out.println("NexoraClient 1.21.1 is now Active!");
+    }
+}
 
