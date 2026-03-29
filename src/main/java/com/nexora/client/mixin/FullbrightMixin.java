7574452c074a0815bf8817b3ee7c71742eecaf01
Zenit-yapper <zenitog69@gmail.com>
2026-03-29 23:57:33 +0530

    Update FullbrightMixin.java

--- src/main/java/com/nexora/client/mixin/FullbrightMixin.java
+++ src/main/java/com/nexora/client/mixin/FullbrightMixin.java
@@ -1,16 +1,18 @@
 package com.nexora.client.mixin;
 
+import com.nexora.client.NexoraClient;
 import net.minecraft.client.option.SimpleOption;
+import net.minecraft.client.render.LightmapTextureManager;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(SimpleOption.class)
+@Mixin(LightmapTextureManager.class)
 public class FullbrightMixin {
-    @Inject(method = "getValue", at = @At("HEAD"), cancellable = true)
-    private void forceFullbright(CallbackInfoReturnable<Object> cir) {
-        // This mixin logic is used to override the Gamma setting
+    @Inject(method = "getBrightness", at = @At("HEAD"), cancellable = true)
+    private static void onGetBrightness(CallbackInfoReturnable<Float> cir) {
+        // If you want it always on:
+        cir.setReturnValue(15.0f); 
     }
 }
-
