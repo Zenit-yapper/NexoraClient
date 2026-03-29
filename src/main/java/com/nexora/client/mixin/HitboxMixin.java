4c0b9c6c455fe0eec4a29e0664990ea444637bd6
Zenit-yapper <zenitog69@gmail.com>
2026-03-29 18:37:54 +0530

    Update HitboxMixin.java

--- src/main/java/com/nexora/client/mixin/HitboxMixin.java
+++ src/main/java/com/nexora/client/mixin/HitboxMixin.java
@@ -1,17 +1,7 @@
-package com.nexora.client.mixin;
-
-import com.nexora.client.gui.ClickGuiScreen;
-import net.minecraft.entity.Entity;
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-
 @Mixin(Entity.class)
 public class HitboxMixin {
     @Inject(method = "getTargetingMargin", at = @At("HEAD"), cancellable = true)
     private void onHitbox(CallbackInfoReturnable<Float> cir) {
-        // FIXED: Uses Float to prevent the conversion error in your logs
         cir.setReturnValue(ClickGuiScreen.hitboxSize);
     }
 }
