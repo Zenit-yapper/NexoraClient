e07cfc0c58f006665bfb448d06bcff746c91559c
Zenit-yapper <zenitog69@gmail.com>
2026-03-29 20:58:48 +0530

    Update ReachMixin.java

--- src/main/java/com/nexora/client/mixin/ReachMixin.java
+++ src/main/java/com/nexora/client/mixin/ReachMixin.java
@@ -1,1 +1,18 @@
+package com.nexora.client.mixin;
+
+import com.nexora.client.gui.ClickGuiScreen;
+import net.minecraft.entity.player.PlayerEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(PlayerEntity.class)
+public abstract class ReachMixin {
+    @Inject(method = "getEntityInteractionRange", at = @At("HEAD"), cancellable = true)
+    private void onReach(CallbackInfoReturnable<Double> cir) {
+        // Uses the reach value from your ClickGUI
+        cir.setReturnValue((double) ClickGuiScreen.reachDistance);
+    }
+}
 
