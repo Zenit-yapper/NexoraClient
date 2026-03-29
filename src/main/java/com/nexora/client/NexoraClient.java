b33ce6bd0b62f7a5877e7c22d285897ab2d17e96
Zenit-yapper <zenitog69@gmail.com>
2026-03-29 20:23:43 +0530

    Update NexoraClient.java

--- src/main/java/com/nexora/client/NexoraClient.java
+++ src/main/java/com/nexora/client/NexoraClient.java
@@ -2,24 +2,23 @@ package com.nexora.client;
 
 import com.nexora.client.gui.ClickGuiScreen;
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.api.ModInitializer; [span_2](start_span)//[span_2](end_span)
+import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
 import net.minecraft.client.MinecraftClient;
 import org.lwjgl.glfw.GLFW;
 
-[span_3](start_span)// Adding "implements ModInitializer" fixes the crash in your log[span_3](end_span)
 public class NexoraClient implements ClientModInitializer, ModInitializer {
-    public static boolean fullbright = false; 
+    public static boolean fullbright = false;
 
     @Override
     public void onInitialize() {
-        [span_4](start_span)// This method must exist to satisfy the 'main' entrypoint[span_4](end_span)
+        // This method must exist to satisfy the 'main' entrypoint
     }
 
     @Override
     public void onInitializeClient() {
-        // Key listener for Right Shift (Mojo Key Code 6036)
+        // Menu Key Listener (Right Shift)
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
             while (client.options.allKeys[GLFW.GLFW_KEY_RIGHT_SHIFT].wasPressed()) {
                 if (client.player != null && client.currentScreen == null) {
@@ -28,7 +27,7 @@ public class NexoraClient implements ClientModInitializer, ModInitializer {
             }
         });
 
-        // Renders the NEXORA branding and FPS
+        // Branding HUD
         HudRenderCallback.EVENT.register((context, tickDelta) -> {
             MinecraftClient client = MinecraftClient.getInstance();
             if (client.player == null || client.options.hudHidden) return;
