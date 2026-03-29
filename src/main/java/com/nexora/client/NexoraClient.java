040e6e82a4f2c35186eaa36bfa84c49f35f51f09
Zenit-yapper <zenitog69@gmail.com>
2026-03-29 13:33:10 +0530

    Update NexoraClient.java

--- src/main/java/com/nexora/client/NexoraClient.java
+++ src/main/java/com/nexora/client/NexoraClient.java
@@ -1,10 +1,14 @@
 package com.nexora.client;
 
-import net.fabricmc.api.ModInitializer;
+import net.fabricmc.api.ClientModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-public class NexoraClient implements ModInitializer {
+public class NexoraClient implements ClientModInitializer {
+    public static final Logger LOGGER = LoggerFactory.getLogger("nexora-client");
+
     @Override
-    public void onInitialize() {
-        // This makes the mod valid so the game doesn't crash on startup
+    public void onInitializeClient() {
+        LOGGER.info("Nexora Client initialized for 1.21.1!");
     }
 }
