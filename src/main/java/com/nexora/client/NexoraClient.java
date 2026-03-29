cc0affeae1c41477be330129c62f1058e4806733
Zenit-yapper <zenitog69@gmail.com>
2026-03-29 13:07:09 +0530

    Update NexoraClient.java

--- src/main/java/com/nexora/client/NexoraClient.java
+++ src/main/java/com/nexora/client/NexoraClient.java
@@ -1,10 +1,14 @@
 package com.nexora.client;
 
 import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class NexoraClient implements ModInitializer {
+    public static final Logger LOGGER = LoggerFactory.getLogger("nexora-client");
+
     @Override
     public void onInitialize() {
-        // Mod starts here
+        LOGGER.info("Nexora Client has started successfully!");
     }
 }
