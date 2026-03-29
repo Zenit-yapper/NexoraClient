848f98fb17bff16b657049413aa39009e6da6671
Zenit-yapper <zenitog69@gmail.com>
2026-03-29 18:10:26 +0530

    Update ReachMixin.java

--- src/main/java/com/nexora/client/mixin/ReachMixin.java
+++ src/main/java/com/nexora/client/mixin/ReachMixin.java
@@ -1,16 +1,8 @@
-package com.nexora.client.mixin;
-
-import com.nexora.client.gui.ClickGuiScreen;
-import net.minecraft.entity.player.PlayerAbilities;
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-
-@Mixin(PlayerAbilities.class)
-public class ReachMixin {
-    @Inject(method = "getReachDistance", at = @At("HEAD"), cancellable = true)
-    private void onGetReach(CallbackInfoReturnable<Float> cir) {
-        cir.setReturnValue(ClickGuiScreen.reachDistance);
+@Mixin(Entity.class)
+public abstract class ReachMixin {
+    @Inject(method = "getTargetingMargin", at = @At("HEAD"), cancellable = true)
+    private void onHitbox(CallbackInfoReturnable<Float> cir) {
+        // This expands the hitbox of EVERY entity you look at
+        cir.setReturnValue(com.nexora.client.gui.ClickGuiScreen.hitboxSize);
     }
 }
