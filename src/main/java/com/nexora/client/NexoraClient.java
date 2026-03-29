c587c8d30a21b6a24245edfc1c2e88d3ee78716a
Zenit-yapper <zenitog69@gmail.com>
2026-03-29 17:36:43 +0530

    Update NexoraClient.java

--- src/main/java/com/nexora/client/NexoraClient.java
+++ src/main/java/com/nexora/client/NexoraClient.java
@@ -1,25 +1,31 @@
 package com.nexora.client;
 
+import com.nexora.client.gui.ClickGuiScreen;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.api.ClientModInitializer;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
+import org.lwjgl.glfw.GLFW;
 
-// You MUST implement both for your current fabric.mod.json to work
 public class NexoraClient implements ModInitializer, ClientModInitializer {
-    public static final Logger LOGGER = LoggerFactory.getLogger("nexora-client");
-    
-    // Toggles for ClickGUI features
     public static boolean fullbright = false;
-    public static boolean fastRender = true;
+    public static KeyBinding guiKey;
 
     @Override
-    public void onInitialize() {
-        LOGGER.info("Nexora Main Entrypoint Initialized");
-    }
+    public void onInitialize() {}
 
     @Override
     public void onInitializeClient() {
-        LOGGER.info("Nexora Client Entrypoint Initialized");
+        guiKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+                "key.nexora.gui", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_RIGHT_SHIFT, "Nexora"
+        ));
+
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            while (guiKey.wasPressed()) {
+                client.setScreen(new ClickGuiScreen());
+            }
+        });
     }
 }
