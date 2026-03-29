c262b1c587a5553002df77ea0bc5f551c452094c
Zenit-yapper <zenitog69@gmail.com>
2026-03-29 17:25:46 +0530

    Update NexoraClient.java

--- src/main/java/com/nexora/client/NexoraClient.java
+++ src/main/java/com/nexora/client/NexoraClient.java
@@ -1,40 +1,25 @@
 package com.nexora.client;
 
-import com.nexora.client.gui.ClickGuiScreen;
-import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
-import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.option.KeyBinding;
-import net.minecraft.client.util.InputUtil;
-import org.lwjgl.glfw.GLFW;
+import net.fabricmc.api.ClientModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+// You MUST implement both for your current fabric.mod.json to work
 public class NexoraClient implements ModInitializer, ClientModInitializer {
+    public static final Logger LOGGER = LoggerFactory.getLogger("nexora-client");
+    
+    // Toggles for ClickGUI features
     public static boolean fullbright = false;
-    public static boolean fastRender = true; // FPS Boost toggle
-    public static KeyBinding guiKey;
+    public static boolean fastRender = true;
 
     @Override
-    public void onInitialize() {}
+    public void onInitialize() {
+        LOGGER.info("Nexora Main Entrypoint Initialized");
+    }
 
     @Override
     public void onInitializeClient() {
-        // 1. Register Key
-        guiKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
-                "key.nexora.gui", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_RIGHT_SHIFT, "Nexora"
-        ));
-
-        // 2. GUI & Logic Tick
-        ClientTickEvents.END_CLIENT_TICK.register(client -> {
-            while (guiKey.wasPressed()) {
-                client.setScreen(new ClickGuiScreen());
-            }
-            
-            // FPS Boost: Force low-frequency cloud rendering if enabled
-            if (fastRender && client.options != null) {
-                client.options.getCloudRenderMode().setValue(net.minecraft.client.option.CloudRenderMode.OFF);
-            }
-        });
+        LOGGER.info("Nexora Client Entrypoint Initialized");
     }
 }
