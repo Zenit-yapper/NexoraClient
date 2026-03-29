bd4730872e99f6738d72e210628382143d68fc21
Zenit-yapper <zenitog69@gmail.com>
2026-03-29 18:08:54 +0530

    Update NexoraClient.java

--- src/main/java/com/nexora/client/NexoraClient.java
+++ src/main/java/com/nexora/client/NexoraClient.java
@@ -1,10 +1,11 @@
 package com.nexora.client;
 
 import com.nexora.client.gui.ClickGuiScreen;
-import net.fabricmc.api.ModInitializer;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
 import org.lwjgl.glfw.GLFW;
@@ -14,13 +15,10 @@ public class NexoraClient implements ModInitializer, ClientModInitializer {
     public static KeyBinding guiKey;
 
     @Override
-    public void onInitialize() {
-        // Required for 'main' entrypoint
-    }
+    public void onInitialize() {}
 
     @Override
     public void onInitializeClient() {
-        // Required for 'client' entrypoint
         guiKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
                 "key.nexora.gui", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_RIGHT_SHIFT, "Nexora"
         ));
@@ -28,6 +26,11 @@ public class NexoraClient implements ModInitializer, ClientModInitializer {
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
             while (guiKey.wasPressed()) {
                 client.setScreen(new ClickGuiScreen());
+            }
+
+            // FULLBRIGHT LOGIC: Force Gamma to 1000%
+            if (fullbright && client.options != null) {
+                client.options.getGamma().setValue(10.0); 
             }
         });
     }
