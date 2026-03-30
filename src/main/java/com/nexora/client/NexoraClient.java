89b90e6c4e1658b990e30d4fc3d3416934738088
Zenit-yapper <zenitog69@gmail.com>
2026-03-30 08:43:00 +0530

    Update NexoraClient.java

--- src/main/java/com/nexora/client/NexoraClient.java
+++ src/main/java/com/nexora/client/NexoraClient.java
@@ -11,13 +11,13 @@ public class NexoraClient implements ModInitializer, ClientModInitializer {
 
     @Override
     public void onInitialize() {
-        // This is the Main Entrypoint
-        LOGGER.info("Nexora Client Main Initialized!");
+        // This satisfies the 'main' entrypoint requirement
+        LOGGER.info("Nexora Client: Main System Initialized!");
     }
 
     @Override
     public void onInitializeClient() {
-        // This is the Client Entrypoint
-        LOGGER.info("Nexora Client Side Initialized!");
+        // This is where your HUD and Client-side features start
+        LOGGER.info("Nexora Client: Client Graphics Initialized!");
     }
 }
