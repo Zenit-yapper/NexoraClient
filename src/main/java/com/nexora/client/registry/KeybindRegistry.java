253d5cc7071610ffb59ee34af70875b50fdac5d1
Zenit-yapper <zenitog69@gmail.com>
2026-03-30 18:08:43 +0530

    Update KeybindRegistry.java

--- src/main/java/com/nexora/client/registry/KeybindRegistry.java
+++ src/main/java/com/nexora/client/registry/KeybindRegistry.java
@@ -1,20 +1,30 @@
 package com.nexora.client.registry;
 
+import com.nexora.client.gui.ClickGuiScreen;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
 import org.lwjgl.glfw.GLFW;
 
 public class KeybindRegistry {
-    public static KeyBinding clickGuiKey;
+    public static KeyBinding openGuiKey;
 
     public static void register() {
-        clickGuiKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
-                "key.nexora.clickgui", 
-                InputUtil.Type.KEYSYM, 
-                GLFW.GLFW_KEY_RIGHT_SHIFT, 
+        // 1. Define the Right Shift key
+        openGuiKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+                "key.nexora.open_gui",
+                InputUtil.Type.KEYSYM,
+                GLFW.GLFW_KEY_RIGHT_SHIFT,
                 "category.nexora.client"
         ));
+
+        // 2. Listen for the press
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            while (openGuiKey.wasPressed()) {
+                // This opens the GUI we made earlier
+                client.setScreen(new ClickGuiScreen());
+            }
+        });
     }
 }
-
