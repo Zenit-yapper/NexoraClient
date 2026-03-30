e50dde54d8c621b8d05165c91cc0a2a6110894c8
Zenit-yapper <zenitog69@gmail.com>
2026-03-30 18:08:15 +0530

    Update NexoraClient.java

--- src/main/java/com/nexora/client/NexoraClient.java
+++ src/main/java/com/nexora/client/NexoraClient.java
@@ -1,26 +1,18 @@
 package com.nexora.client;
 
-import com.nexora.client.registry.KeybindingRegistry; // Import your registry folder
+import com.nexora.client.registry.KeybindRegistry; // Fixed name: Keybind (no 'ing')
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.ModInitializer;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class NexoraClient implements ModInitializer, ClientModInitializer {
-    public static final String MOD_ID = "nexora_client";
-    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
-
     @Override
     public void onInitialize() {
-        LOGGER.info("Nexora Client: Main System Initialized!");
+        // Main init
     }
 
     @Override
     public void onInitializeClient() {
-        // This is the missing piece! 
-        // It tells your registry folder to start listening for Right Shift.
-        KeybindingRegistry.register(); 
-
-        LOGGER.info("Nexora Client: Client-Side Initialized and Registry Loaded!");
+        // Start your keybind listener
+        KeybindRegistry.register(); 
     }
 }
