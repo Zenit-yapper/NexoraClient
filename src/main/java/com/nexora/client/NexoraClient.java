c7b45877a46585503b0eb1a4e2dd021ba8e2ca5c
Zenit-yapper <zenitog69@gmail.com>
2026-03-30 17:58:26 +0530

    Update NexoraClient.java

--- src/main/java/com/nexora/client/NexoraClient.java
+++ src/main/java/com/nexora/client/NexoraClient.java
@@ -1,7 +1,13 @@
 package com.nexora.client;
 
+import com.nexora.client.gui.ClickGuiScreen;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
+import org.lwjgl.glfw.GLFW;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -9,15 +15,30 @@ public class NexoraClient implements ModInitializer, ClientModInitializer {
     public static final String MOD_ID = "nexora_client";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
+    private static KeyBinding guiKeyBinding;
+
     @Override
     public void onInitialize() {
-        // This handles the Main entrypoint for the Fabric Loader
-        LOGGER.info("Nexora Client: Main System Initialized!");
+        [span_3](start_span)LOGGER.info("Nexora Client: Main System Initialized!");[span_3](end_span)
     }
 
     @Override
     public void onInitializeClient() {
-        // This handles the Client-side features (HUD, Mixins, etc.)
-        LOGGER.info("Nexora Client: Client-Side Initialized!");
+        // 1. Create the KeyBinding for Right Shift
+        guiKeyBinding = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+                "key.nexora.open_gui", 
+                InputUtil.Type.KEYSYM, 
+                GLFW.GLFW_KEY_RIGHT_SHIFT, 
+                "category.nexora.client"
+        ));
+
+        // 2. Listen for the key press every "Tick"
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            while (guiKeyBinding.wasPressed()) {
+                client.setScreen(new ClickGuiScreen());
+            }
+        });
+
+        [span_4](start_span)LOGGER.info("Nexora Client: Keybinds and GUI Registered!");[span_4](end_span)
     }
 }
