8fe4cc5a06c7c72c1e6d016f8b97bb94efa06b85
Zenit-yapper <zenitog69@gmail.com>
2026-03-30 18:18:45 +0530

    Update NexoraClient.java

--- src/main/java/com/nexora/client/NexoraClient.java
+++ src/main/java/com/nexora/client/NexoraClient.java
@@ -1,18 +1,15 @@
 package com.nexora.client;
 
-import com.nexora.client.registry.KeybindRegistry; // Fixed name: Keybind (no 'ing')
+import com.nexora.client.registry.KeybindRegistry;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.ModInitializer;
 
 public class NexoraClient implements ModInitializer, ClientModInitializer {
     @Override
-    public void onInitialize() {
-        // Main init
-    }
+    public void onInitialize() {}
 
     @Override
     public void onInitializeClient() {
-        // Start your keybind listener
         KeybindRegistry.register(); 
     }
 }
