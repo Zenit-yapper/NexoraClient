b28a2b081ac5b49dc2c4adfc0787bc6018fa0a3d
Zenit-yapper <zenitog69@gmail.com>
2026-03-31 20:26:04 +0530

    Update NexoraClient.java

--- src/main/java/com/nexora/client/NexoraClient.java
+++ src/main/java/com/nexora/client/NexoraClient.java
@@ -1,15 +1,28 @@
 package com.nexora.client;
 
-import com.nexora.client.registry.KeybindRegistry;
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
+import org.lwjgl.glfw.GLFW;
 
-public class NexoraClient implements ModInitializer, ClientModInitializer {
-    @Override
-    public void onInitialize() {}
+public class NexoraClient implements ClientModInitializer {
+    public static boolean isZooming = false;
+    private static KeyBinding zoomKey;
 
     @Override
     public void onInitializeClient() {
-        KeybindRegistry.register(); 
+        // Register the Zoom Key (Default: C)
+        zoomKey = new KeyBinding(
+            "key.nexora.zoom", 
+            InputUtil.Type.KEYSYM, 
+            GLFW.GLFW_KEY_C, 
+            "category.nexora.client"
+        );
+
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            // Update the zooming state every tick
+            isZooming = zoomKey.isPressed();
+        });
     }
 }
