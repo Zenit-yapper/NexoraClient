1532f8e64ae89375eff75490174147a3738171fa
Zenit-yapper <zenitog69@gmail.com>
2026-03-31 18:26:37 +0530

    Create GameRendererMixin.java

---
+++ src/main/java/com/nexora/client/mixin/GameRendererMixin.java
@@ -0,0 +1,17 @@
+package com.nexora.client.mixin;
+
+import net.minecraft.client.render.GameRenderer;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(GameRenderer.class)
+public class GameRendererMixin {
+    @Inject(method = "loadPostProcessor", at = @At("HEAD"), cancellable = true)
+    private void stopShaders(CallbackInfo ci) {
+        // This force-cancels the blur shader before it even starts.
+        ci.cancel();
+    }
+}
+
