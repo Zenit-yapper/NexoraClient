e1e34861dd52d9d7b2347ab3fbe2ce20c9c2fa0e
Zenit-yapper <zenitog69@gmail.com>
2026-03-31 22:07:06 +0530

    Update GameRendererMixin.java

--- src/main/java/com/nexora/client/mixin/GameRendererMixin.java
+++ src/main/java/com/nexora/client/mixin/GameRendererMixin.java
@@ -1,12 +1,19 @@
-package com.nexora.client;
+package com.nexora.client.mixin;
 
-import net.fabricmc.api.ModInitializer;
+import net.minecraft.client.render.GameRenderer;
+import net.minecraft.util.Identifier;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-public class NexoraClient implements ModInitializer {
-
-    @Override
-    public void onInitialize() {
-        // All zoom keybinds and logic have been removed.
-        System.out.println("Nexora Client: Zoom and restricted mods removed.");
+@Mixin(GameRenderer.class)
+public class GameRendererMixin {
+    @Inject(method = "loadPostProcessor", at = @At("HEAD"), cancellable = true)
+    private void stopVanillaBlur(Identifier id, CallbackInfo ci) {
+        // This is what stops the GUI from being blurred
+        if (id.getPath().contains("blur")) {
+            ci.cancel();
+        }
     }
 }
