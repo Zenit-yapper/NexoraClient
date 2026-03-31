bbe05d52ba9527b2f55670488bbed61b54ebb9a6
Zenit-yapper <zenitog69@gmail.com>
2026-03-31 21:46:08 +0530

    Update NexoraClient.java

--- src/main/java/com/nexora/client/NexoraClient.java
+++ src/main/java/com/nexora/client/NexoraClient.java
@@ -1,29 +1,12 @@
 package com.nexora.client;
 
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
-import net.minecraft.client.option.KeyBinding;
-import net.minecraft.client.util.InputUtil;
-import org.lwjgl.glfw.GLFW;
 
 public class NexoraClient implements ModInitializer {
-    public static boolean isZooming = false;
-    private static KeyBinding zoomKey;
 
     @Override
     public void onInitialize() {
-        // Register the Zoom Key (Default: C)
-        zoomKey = new KeyBinding(
-            "key.nexora.zoom", 
-            InputUtil.Type.KEYSYM, 
-            GLFW.GLFW_KEY_C, 
-            "category.nexora.client"
-        );
-
-        ClientTickEvents.END_CLIENT_TICK.register(client -> {
-            isZooming = zoomKey.isPressed();
-        });
-
-        System.out.println("Nexora Client 1.0.0 Initialized Successfully!");
+        // All zoom keybinds and logic have been removed.
+        System.out.println("Nexora Client: Zoom and restricted mods removed.");
     }
 }
